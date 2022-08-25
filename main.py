f30b27e54ce2e0bb3fbe946bb9ad29df5e5d0dd2
imahdimir <imahdimir@gmail.com>
2022-08-25 17:36:18 +0430

    init

--- main.py
+++ main.py
@@ -4,33 +4,30 @@
 
   """
 
-
-import pandas as pd
 from githubdata import GithubData
 import requests
 import json
 
+
 dlist_repo_url = 'https://github.com/imahdimir/d-Datasets-list'
-cur_repo_url =
+cur_repo_url = 'https://github.com/imahdimir/gov-d-Datasets-list'
 
 desc = 'description'
+rgburl = 'https://raw.github.com/'
+gitburl = 'https://github.com/'
 
-
-
-
-def clone_read_description_remove(url):
+def read_description_remove_from_meta(url) :
   print(url)
 
   r = requests.get(url)
   meta = r.json()
 
-  if desc in meta.keys():
+  if desc in meta.keys() :
     _desc = meta[desc]
-  else:
+  else :
     _desc = None
 
   return _desc
-
 
 def main() :
   pass
@@ -41,30 +38,40 @@ def main() :
   ##
   djs = drepo.meta
   ##
-  for url in djs.keys():
-    djs[url] = clone_read_description_remove(url)
+  for url in djs.keys() :
+    djs[url] = read_description_remove_from_meta(url)
   ##
   djsfp = drepo.meta_filepath
-  with open(djsfp, 'w') as fi:
-    json.dump(djs, fi)
+  with open(djsfp , 'w') as fi :
+    json.dump(djs , fi)
+
   ##
-  msg = 'read meta files'
+  rdme = '| DataSet | Short Description |\n'
+  rdme += '| --- | --- |\n'
+
+  for ky , val in djs.items() :
+    rpun = ky.replace(rgburl , '')
+    rpunspl = rpun.split('/')
+    rpun = '/'.join(rpunspl[:2])
+    _url = gitburl + rpun
+    rpn = rpunspl[1].split('d-' , 2)[1]
+    nln = '| [' + rpn + '](' + _url + ') | ' + val + ' |\n'
+    print(nln)
+    rdme += nln
+  ##
+  rdmefp = drepo.local_path / 'README.md'
+
+  with open(rdmefp , 'w') as fi :
+    fi.write(rdme)
+  ##
+  msg = 'updated README.md'
+  msg += ' by: ' + cur_repo_url
+
   drepo.commit_push(msg)
+
   ##
   drepo.rmdir()
-  ##
-
-
-
-
-
-
-
-
 
   ##
 
-
-
-##
 ##
