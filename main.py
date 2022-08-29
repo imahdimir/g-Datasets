164e7dedf13ce9a908e879fa319c34a1419c7f51
imahdimir <imahdimir@gmail.com>
2022-08-29 17:58:20 +0430

    async

--- main.py
+++ main.py
@@ -3,15 +3,13 @@
 
 import asyncio
 from ast import literal_eval
+from functools import partial
 
-import nest_asyncio
 import pandas as pd
-from aiohttp import ClientSession
 from githubdata import GithubData
-from mirutil import funcs as mf
+from mirutil import async_requests as areq
+from mirutil import utils as mu
 
-
-nest_asyncio.apply()
 
 dlist_repo_url = 'https://github.com/imahdimir/Datasets'
 
@@ -22,33 +20,35 @@ desc = 'description'
 meturl = 'metaurl'
 meta = 'meta'
 
-async def get_json_by_its_url(url) :
-  async with ClientSession() as ses :
-    async with ses.get(url) as resp :
-      return await resp.json(content_type = None)
 
-async def a_task(url) :
-  jsn = await get_json_by_its_url(url)
-  return await read_desc_in_meta_jsn(jsn)
-
-async def read_desc_in_meta_jsn(jsn) :
+def read_desc_in_meta_jsn(jsn) :
   if desc in jsn.keys() :
     return jsn[desc]
   else :
     return None
 
+
 async def read_main(urls) :
-  tasks = [a_task(x) for x in urls]
-  return await asyncio.gather(*tasks)
+  fu = partial(areq.get_reps_jsons_async ,
+               trust_env = False ,
+               params = None ,
+               verify_ssl = True ,
+               content_type = None)
+  jsns = await fu(urls)
+  out = [read_desc_in_meta_jsn(x) for x in jsns]
+  return out
+
 
 def get_dataset_name_from_url(url) :
   repon = url.split('/')[-1]
   return repon.split('d-' , 1)[1]
 
+
 def main() :
   pass
 
   ##
+
   drp = GithubData(dlist_repo_url)
   drp.clone()
 
@@ -63,13 +63,14 @@ def main() :
 
   ##
   df = pd.DataFrame(data = js.keys())
+  df = df.drop_duplicates()
 
   ##
   df[meturl] = df[0].str.replace(gitburl , rgburl)
   df[meturl] = df[meturl] + '/main/META.json'
 
   ##
-  cis = mf.return_clusters_indices(df)
+  cis = mu.return_clusters_indices(df)
 
   ##
   df = df.reset_index(drop = True)
@@ -107,13 +108,16 @@ def main() :
     fi.write(rdme)
 
   ##
+  tokfp = '/Users/mahdi/Dropbox/tok.txt'
+  tok = mu.get_tok_if_accessible(tokfp)
+
+  ##
   msg = 'updated README.md'
   msg += ' by: ' + cur_repo_url
 
-  drp.commit_push(msg)
+  drp.commit_push(msg , usr = drp.usr , token = tok)
 
   ##
-
 
   drp.rmdir()
 
